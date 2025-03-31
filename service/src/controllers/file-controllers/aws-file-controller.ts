83731b287f9677801233a62aac68b45dde7e2178
Behnam Tehrani <behnam777@gmail.com>
2025-03-31 14:22:51 -0400

    upload to gcp

---
+++ service/src/controllers/file-controllers/aws-file-controller.ts
@@ -0,0 +1,40 @@
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { logger } from '../../utils/logger.utils';
+import { FileController } from '../file-controller';
+
+export class AWSFileController implements FileController {
+  private s3Client: S3Client;
+  private bucket: string;
+
+  constructor() {
+    this.bucket = process.env.AWS_S3_BUCKET || '';
+    this.s3Client = new S3Client({
+      region: process.env.AWS_REGION || 'us-east-1',
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+      },
+    });
+  }
+
+  async uploadFile(file: Express.Multer.File): Promise<string> {
+    try {
+      const key = `uploads/${Date.now()}-${file.originalname}`;
+      
+      const command = new PutObjectCommand({
+        Bucket: this.bucket,
+        Key: key,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+      });
+
+      await this.s3Client.send(command);
+      
+      // Return the public URL of the uploaded file
+      return `https://${this.bucket}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`;
+    } catch (error) {
+      logger.error('Failed to upload file to S3:', error);
+      throw new Error('Failed to upload file');
+    }
+  }
+}
