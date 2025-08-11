bbd1127287bfd460939001e5341a7789b1c55d68
Behnam Tehrani <behnam777@gmail.com>
2025-08-11 08:34:10 -0400

    remove other unused app

--- cms/test-application/src/index.tsx
+++
@@ -1,8 +0,0 @@
-/// <reference types="@commercetools-frontend/application-config/client" />
-
-import { createRoot } from 'react-dom/client';
-import EntryPoint from './components/entry-point';
-
-const container = document.getElementById('app');
-const root = createRoot(container as Element);
-root.render(<EntryPoint />);
