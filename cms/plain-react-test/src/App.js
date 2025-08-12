ee9ac25454b5ee3f302c43e034f3cf79c464df45
Behnam Tehrani <behnam777@gmail.com>
2025-08-12 17:04:44 -0400

    update plain-test app

--- cms/plain-react-test/src/App.js
+++ cms/plain-react-test/src/App.js
@@ -1,16 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import ContentItem from '@commercetools-demo/cms-content-items';
+import ContentItem from '@commercetools-demo/contentools-content-items';
+import ContentType from '@commercetools-demo/contentools-content-types';
 
 function App() {
   return (
     <div className="App">
       <ContentItem
-          baseURL="http://localhost:8080/service"
-          businessUnitKey="business_unit_behnam"
-          locale="en-US"
-          parentUrl="content-items"
-        />
+        baseURL="http://localhost:8080/service"
+        businessUnitKey="business_unit_behnam"
+        locale="en-US"
+        parentUrl="content-items"
+      />
+      <ContentType
+        baseURL="http://localhost:8080/service"
+        businessUnitKey="business_unit_behnam"
+        locale="en-US"
+        parentUrl="content-types"
+      />
     </div>
   );
 }
