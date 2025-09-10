f4c60d2b771ac47e87f2f132e5e97296ad99aa02
Behnam Tehrani <behnam777@gmail.com>
2025-09-10 13:49:51 -0400

    Reomve assets directory

--- assets/src/apps/content-type-app/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ContentTypeForm } from './content-type-form';
-export { default as ContentTypeTable } from './content-type-table';
-export { default as ContentTypeHeader } from './content-type-header';
-export { default as ErrorMessage } from './error-message';
--- assets/src/apps/content-type-registry/sample-content-type-renderers/index.ts
+++
@@ -1,9 +0,0 @@
-import './hero-banner';
-import './product-slider';
-import './rich-text';
-import './website-logo';
-
-export { HeroBanner } from './hero-banner';
-export { ProductSlider } from './product-slider';
-export { RichText } from './rich-text';
-export { WebsiteLogo } from './website-logo';
