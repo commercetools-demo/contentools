86b658f2c0966c96d10f4dfeba24a9cd97f73668
Behnam Tehrani <behnam777@gmail.com>
2025-09-16 11:00:10 -0400

    update test app

--- cms/plain-react-test/src/App.js
+++ cms/plain-react-test/src/App.js
@@ -3,22 +3,43 @@ import './App.css';
 import ContentItem from '@commercetools-demo/contentools-content-items';
 import ContentType from '@commercetools-demo/contentools-content-types';
 import ContentItemRenderer from '@commercetools-demo/contentools-content-item-renderer';
+import ContentPages from '@commercetools-demo/contentools-content-pages';
+import PageRenderer from '@commercetools-demo/contentools-page-renderer';
 
 function App() {
   return (
     <div className="App">
       <ContentItem
         baseURL="http://localhost:8080/service"
-        businessUnitKey="business_unit_behnam"
-        locale="en-US"
-        parentUrl="content-items"
+        businessUnitKey="central-texas-animal-hospital"
+        locale="en-us"
+        parentUrl=""
       />
       <ContentType
         baseURL="http://localhost:8080/service"
-        businessUnitKey="business_unit_behnam"
-        locale="en-US"
-        parentUrl="content-types"
+        businessUnitKey="central-texas-animal-hospital"
+        locale="en-us"
+        parentUrl=""
       />
+      {/* <ContentPages
+        baseURL="http://localhost:8080/service"
+        businessUnitKey="central-texas-animal-hospital"
+        locale="en-US"
+        parentUrl="content-pages"
+      /> */}
+      {/* <PageRenderer
+        baseURL="http://localhost:8080/service"
+        businessUnitKey="central-texas-animal-hospital"
+        locale="en-US"
+        parentUrl="content-pages"
+        isDraft={true}
+        // pageKey="page-b159017d-9baa-435f-b920-04c7db95bf9e"
+        query='route="/asdsd"'
+        style={{
+          '--page-grid-gap': '0.5rem',
+          '--page-grid-row-margin': '0.5rem',
+        }}
+      /> */}
       {/* <ContentItemRenderer
         baseURL="http://localhost:8080/service"
         businessUnitKey="business_unit_behnam"
